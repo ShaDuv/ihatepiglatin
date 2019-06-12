fa315637e37f735b6c7e5aa88c65478329079d9b
Everett Lee-Wuollet, Kira Roberts and Shawna Duvall <kira@>
2019-06-12 15:59:59 -0700

    adds js to check each first letter of the words

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   var vowels = ["a", "e", "i", "o", "u"];
+
   //This collects data from user and splits it into individual words.
   $('#pigbutton').click(function (event) {
     var words = ($('input#original').val()).split(' ');
@@ -8,7 +9,12 @@ $(document).ready(function () {
     //This takes each word and breaks it into letters
     for (i = 0; i < words.length; i++) {
       var chars = words[i].split('');
-      console.log(chars);
-    }
+      if (vowels.includes(words[i][0])=== true){
+      console.log(words[i][0]);
+};
+      // if (vowels.includes(chars[i])) {
+        console.log('this is working');
+      // };
+    };
   });
 });
