65d0e5544121548ef9e1004480ede7eb59ceba50
Everett Lee-Wuollet, Kira Roberts and Shawna Duvall <shawna@>
2019-06-12 16:34:24 -0700

    identify first letters and consonant or vowel and display result in console

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   var vowels = ["a", "e", "i", "o", "u"];
-
+  var conson = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
   //This collects data from user and splits it into individual words.
   $('#pigbutton').click(function (event) {
     var words = ($('input#original').val()).split(' ');
@@ -8,13 +8,15 @@ $(document).ready(function () {
 
     //This takes each word and breaks it into letters
     for (i = 0; i < words.length; i++) {
-      var chars = words[i].split('');
+      // var chars = words[i].split('');
       if (vowels.includes(words[i][0])=== true){
-      console.log(words[i][0]);
-};
-      // if (vowels.includes(chars[i])) {
-        console.log('this is working');
-      // };
-    };
-  });
+        console.log("vowels");
+      } else if(conson.includes(words[i][0])=== true); {
+          // console.log(words[i][0]);
+          console.log("consononly")
+          // if (vowels.includes(chars[i])) {
+          // };
+        };
+      };
+      });
 });
