3f252917932301bf06d505284a0c2810323f1d4c
Shawna Duvall <shawnaduvall@gmail.com@>
2019-06-14 15:19:19 -0700

    This will add more variables for storing our results

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,10 @@ $(document).ready(function () {
   var vowels = ["a", "e", "i", "o", "u"];
   var conson = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
   //This collects data from user and splits it into individual words.
+  var pigLatin = [];
+
+  var i = 0
+
   $('#pigbutton').click(function (event) {
     var words = ($('input#original').val()).split(' ');
     console.log(words);
